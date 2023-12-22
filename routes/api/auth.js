541d29c2e6d848002e52a6e8709e724c2287ba2b
neozxin <neozxin@hotmail.com>
2023-12-22 02:46:41 +0000

    Update 14 files

---
+++ routes/api/auth.js
@@ -0,0 +1,62 @@
+const express = require("express");
+const router = express.Router();
+const { body, validationResult } = require("express-validator");
+const auth = require("../../middleware/auth");
+const User = require("../../models/user");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const config = require("config");
+
+router.get("/", auth, async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id).select("-password");
+    res.json(user);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server Error");
+    // res.send("Auth route");
+  }
+});
+
+router.post(
+  "/",
+  [
+    body("email", "Please include a valid email").isEmail(),
+    body("password", "Password is required").exists(),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    const { email, password } = req.body;
+    try {
+      const user = await User.findOne({ email });
+      const isMatch = await bcrypt.compare(password, user?.password || "");
+      if (!user || !isMatch) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: "Invalid Credentials" }] });
+      }
+      const payload = {
+        user: {
+          id: user.id,
+        },
+      };
+      jwt.sign(
+        payload,
+        config.get("jwtSecret"),
+        { expiresIn: `${60}m` },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        },
+      );
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send("Server error");
+    }
+  },
+);
+
+module.exports = router;
